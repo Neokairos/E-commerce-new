f6319d5a171655d884b8394d7e260408b1e958b1
Neokairos <neokairos@neokairos.neokairos.com>
2024-02-07 23:03:17 -0400

    added user/usermanager to models

--- back/backend/api/models.py
+++ back/backend/api/models.py
@@ -1,5 +1,68 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+import uuid
+from typing import Any, Optional
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserManager(BaseUserManager):
+    def create_user(self,username:str, email:str, password:Optional[str]=None) -> 'User':
+        
+        if username is None:
+            raise TypeError('Users must have an username')
+
+        if email is None:
+            raise TypeError('Users must have an email nigga')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        
+        return user
+    
+    def create_sudo(self,username:str,email:str,password:str) -> "User":
+        
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+        
+        user = self.create_user(username,email,password)
+        user.is_superuser = True
+        user.is_active = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser,PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_lengh=255, unique=True)
+    email = models.EmailField(db_index=True,unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DatetimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    birth_date = models.DateField(null=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        string = self.email if self.email != '' else self.get_user_name()
+        return f'{self.id} {string}'
+
+    @property
+    def tokens(self) -> dict[str,str]:
+        """Allow us to get a user's token by calling `user.token`."""
+        refresh = RefreshToken.for_user(self)
+        return {'refresh': str(refresh), 'access': str(refresh.access_token)}
+    
+
+
 
 class Product(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
--- front/frontend/src/routes/shop/[id]/+page.svelte
+++ front/frontend/src/routes/shop/[id]/+page.svelte
@@ -20,7 +20,7 @@
 				const endpoint = `http://127.0.0.1:8000/api/products/${data.id}`;
 				let res = await axios.get(endpoint);
 				product = res.data;
-				productStore.update((n) => ({ ...n, all: [...n.all, product], current: product })); 
+				productStore.update((n) => ({ ...n, all: [...n.all, product], current: product }));
 				loading = false;
 			}
 		} catch (err) {
@@ -31,24 +31,27 @@
 	});
 </script>
 
-{#if loading}
-	<div class="center">
-		<div class="loader" role="status"></div>
-	</div>
-{:else if !loading}
-	<h2 class="mb-4">{product.title}</h2>
-	<div class="row mt-4">
-		<div class="col-12 col-md-4">
-			<img src={product.image} alt="product" class="w-100 mb-3" />
-			<h5 class="mt-4">Seller: {product.seller}</h5>
-			<a href="/shop/{product.id}/buy" class="btn btn-primary p-8 mt-2 pl-5">Buy</a>
-			<a href="/shop" class="btn btn-danger p-8 mt-2 pr-5">Back</a>
+
+	{#if loading}
+		<div class="center">
+			<div class="loader" role="status"></div>
 		</div>
-		<div class="col-12 col-md-8 py-2">
-			<p class="mb-2">{product.description}</p>
+	{:else if !loading}
+		<h2 class="mb-4">{product.title}</h2>
+		<div class="row mt-4">
+			<div class="col-12 col-md-4">
+				<img src={product.image} alt="product" class="w-100 mb-3" />
+				<h5 class="mt-4">Seller: {product.seller}</h5>
+				<a href="/shop/{product.id}/buy" class="btn btn-primary p-8 mt-2 pl-5">Buy</a>
+				<a href="/shop" class="btn btn-danger p-8 mt-2 pr-5">Back</a>
+			</div>
+			<div class="col-12 col-md-8 py-2">
+				<p class="mb-2">{product.description}</p>
+			</div>
 		</div>
-	</div>
-{:else}
-	<p>No product was found with ID {data.id}</p>
-	<a href="/shop" class="btn btn-danger p-8 mt-2 pr-5">Back</a>
-{/if}
+	{:else}
+		<p>No product was found with ID {data.id}</p>
+		<a href="/shop" class="btn btn-danger p-8 mt-2 pr-5">Back</a>
+	{/if}
+
+
