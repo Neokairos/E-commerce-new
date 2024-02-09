114aa62cecdd99921e48fa4635b2d5ac479ca352
Neokairos <neokairos@neokairos.neokairos.com>
2024-02-08 23:03:13 -0400

    added registration serializer and view

--- back/backend/api/serializers.py
+++ back/backend/api/serializers.py
@@ -1,5 +1,48 @@
 from rest_framework import serializers
 from .models import Product
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from .models import User
+from .utils import validate_email as email_is_valid
+
+
+class RegistrationSerializer(serializers.ModelSerializer[User]):
+    
+    password = serializers.CharField(max_length=20,min_length=4,write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['username',
+                'email',
+                'password',
+                'is_staff',
+                'is_active',
+                'created_at',
+                'updated_at',
+                'birth_date']
+
+    def validate_email(self, value:str) -> str:
+        valid, error_text = email_is_valid(value)
+        if not valid:
+        #if valid is False
+            raise serializers.ValidationError(error_text)
+        try:
+            #splitting the email in two vars and starting it from @
+            email_name, domain_part = value.strip().rsplit('@',1)
+        except ValueError:
+            raise ValueError("Email address must contain exactly one '@' symbol.")
+        else:
+            value = '@'.join([email_name, domain_part.lower()])
+            
+        return value
+            
+    
+
+    def create(self, validated_data):
+        user = user.objects.create_user(
+            username=validated_data['username'], email=validated_data['email'], password=validated_data['password']
+        )
+        return user
 
 class ProductSerializer(serializers.ModelSerializer):
     seller = serializers.ReadOnlyField(source='seller.username')
@@ -12,3 +55,4 @@ class ProductSerializer(serializers.ModelSerializer):
         product = Product.objects.create(**validated_data)
         product.image.save(image.name, image)
         return product
+
--- back/backend/api/views.py
+++ back/backend/api/views.py
@@ -1,8 +1,36 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework import viewsets
+from typing import Any, Optional
+
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from rest_framework import parsers, status
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .serializers import (
+    LoginSerializer,
+    LogoutSerializer,
+    RegistrationSerializer,
+    UserSerializer,
+)
+
+
+
+class RegistrationAPIView(APIView):
+   permission_classes = (AllowAny,)
+   serializer_class = RegistrationSerializer
+
+   def post(self,request: Request) -> Response:
+      user_request = request.data.get('user',{}) #returning an empty object if key pair user not found in dict
+      serializer = self.serializer_class(data=user_request)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ProductViewSet(viewsets.ModelViewSet):
    queryset = Product.objects.all()
